5807c0d5f9fa235dd65c9b07bf88c636283c0537
sysoyev@tut.by <Retalx29>
2020-04-09 23:26:39 +0300

    сборка на mac

--- landing/flutter_service_worker.js
+++ landing/flutter_service_worker.js
@@ -1,18 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "a6496e06d2a483daf1a3bff52c497850",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+  "index.html": "e670e3c1062cdb142a0c64d44ae8274d",
+"/": "e670e3c1062cdb142a0c64d44ae8274d",
+"main.dart.js": "e7f4d373858e659b6959df510f4e302b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d7465d5f0803e8fd07aa0a66c6af666f",
-"/": "d7465d5f0803e8fd07aa0a66c6af666f",
-"main.dart.js": "f3709d91405bbc07f45c9991d3dbc717",
-"manifest.json": "502c7031f696fb474898228586e72ac7"
+"manifest.json": "23409a633d153e3b4a0da065ae1b2541",
+"assets/LICENSE": "ab2aa3d841e136ad7140eae36887ee32",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
 self.addEventListener('activate', function (event) {
