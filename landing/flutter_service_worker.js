5f4bb72e7ca87d7e161bb7cd0a9a82fd7dd5e739
sysoyev <sysoyev@tut.by>
2020-04-13 20:24:32 +0300

    ручная обработка свайпа (и скрола)

--- landing/flutter_service_worker.js
+++ landing/flutter_service_worker.js
@@ -1,18 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "e670e3c1062cdb142a0c64d44ae8274d",
-"/": "e670e3c1062cdb142a0c64d44ae8274d",
-"main.dart.js": "e7f4d373858e659b6959df510f4e302b",
+  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "a6496e06d2a483daf1a3bff52c497850",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "23409a633d153e3b4a0da065ae1b2541",
-"assets/LICENSE": "ab2aa3d841e136ad7140eae36887ee32",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"index.html": "1da1be48d5ec0e07b40e4fb1fbd6d842",
+"/": "1da1be48d5ec0e07b40e4fb1fbd6d842",
+"main.dart.js": "5e5204cb94f25824b5de0a6a1c586555",
+"manifest.json": "d82d4a2b7d7748bab96db21856bac43e"
 };
 
 self.addEventListener('activate', function (event) {
